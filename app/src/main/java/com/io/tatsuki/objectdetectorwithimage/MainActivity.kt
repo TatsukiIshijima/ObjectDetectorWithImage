7f8eadd127e017cae51300d8485b7bdc7ab3ae61
TatsukiIshijima <tatsuki445@gmail.com>
2019-08-15 20:41:35 +0900

    画像の表示までを実装

--- app/src/main/java/com/io/tatsuki/objectdetectorwithimage/MainActivity.kt
+++ app/src/main/java/com/io/tatsuki/objectdetectorwithimage/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.io.tatsuki.objectdetectorwithimage
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val imageView = findViewById<ImageView>(R.id.image_view)
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.dog)
+        imageView.setImageBitmap(bitmap)
     }
 }
